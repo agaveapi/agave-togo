ef7d36b891a621565b46901f2aba2d473527b546
Rion Dooley <deardooley@hotmail.com>
2017-07-04 08:51:31 -0500

    Adding public tenant auth config for demo purposes.

--- auth/scripts/implicit.js
+++ auth/scripts/implicit.js
@@ -8,6 +8,15 @@ var OAuthClients = {
         "supportUrl": "https://support.agaveapi.co",
         "signupUrl": "https://agave-auth.solveij.com/create_account",
         "allowsSignup": false
+    },
+    "agave.prod": {
+        "clientKey": "9fUAucVYyDWffDoaLpZ6u9geHMsa",
+        "callbackUrl": "https://agaveapi.github.io/solveij/auth/",
+        "scope": "PRODUCTION",
+        "projectUrl": "https://agaveapi.co/",
+        "supportUrl": "https://support.agaveapi.co",
+        "signupUrl": "https://public.agaveapi.co/create_account",
+        "allowsSignup": false
     }
 };
 
