0ff14fbe6a5d57d223ea5f9c1aa38db52a5aea24
Rion Dooley <deardooley@hotmail.com>
2017-07-06 00:35:26 -0500

    Cleaning up tenant exclusion filter for context switching on startup.

--- auth/scripts/controllers/LoginFormController.js
+++ auth/scripts/controllers/LoginFormController.js
@@ -235,8 +235,10 @@ angular.module('AgaveAuth').controller('LoginFormController', function ($rootSco
     }
   };
 
-  $scope.excludeSelectedTenant = function (tenant) {
-    return tenant.code !== $scope.selectedTenant.code;
+  $scope.excludeSelectedTenant = function (selectedTenant) {
+    return function(tenant) {
+      return tenant.code !== selectedTenant.code;
+    };
   };
 
   $scope.setCurrentTenant = function (tenant) {
